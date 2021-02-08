d389aee89b3507766f4c1b2c9a23ab9b5a8c8843
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-02-08 15:04:04 +0300

    add env variable 'MONGO_URI'

--- helpers/getDb.js
+++ helpers/getDb.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
 
 try {
-  mongoose.connect(
-    "mongodb+srv://Admin:RhfcfdxbR3416@cluster0.id6e1.mongodb.net/daydreamer?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    }
-  );
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
 } catch (e) {}
 
 const getDB = (name) => {
