ad5ffb902c3802ead32a4995abdfd18dd7c11c8a
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-02-19 10:45:46 +0300

    add big plus button

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import styles from "@/styles/menu.module.scss";
+import { nanoid } from "nanoid";
 
 import TaskEdit from "@/src/components/tasks/TaskEdit";
-
 import ProjectsDropdown from "@/src/components/projects/ProjectsDropdown";
 import Tasks from "@/src/components/tasks/Tasks";
 
+import { TasksContext } from "@/src//context/tasks/TasksContext";
+import { ProjectsContext } from "@/src//context/projects/ProjectsContext";
+
 export default function Menu() {
+  const { tasks, createTask } = useContext(TasksContext);
+  const { projects } = useContext(ProjectsContext);
+  let currentProject = projects.find((project) => project.isCurrent);
+  if (!currentProject) {
+    currentProject = projects[0];
+  }
+
   const [isMenuOpen, setMenu] = useState(false);
   const [isDropdownOpen, setDropdown] = useState(false);
   const [editedTask, setEditedTask] = useState(null);
@@ -14,6 +24,30 @@ export default function Menu() {
   const openMenuHandler = () => {
     setEditedTask(null);
     setMenu(!isMenuOpen);
+  };
+
+  const createTaskHandler = async () => {
+    setMenu(true);
+
+    const currentDate = new Date();
+    let afterWeek = new Date();
+    afterWeek.setDate(currentDate.getDate() + 7);
+
+    const topLevelTasks = tasks.filter(
+      (task) => !task.root && task.project == currentProject._id
+    );
+
+    await createTask({
+      _id: nanoid(),
+      name: "",
+      description: "",
+      dateStart: currentDate,
+      dateEnd: afterWeek,
+      color: "258EFA",
+      project: currentProject._id,
+      root: "",
+      order: topLevelTasks.length,
+    });
   };
 
   return (
@@ -38,6 +72,12 @@ export default function Menu() {
         <Tasks editedTask={editedTask} setEditedTask={setEditedTask} />
         <TaskEdit taskId={editedTask} setEditedTask={setEditedTask} />
       </div>
+      <img
+        src="/img/plus.svg"
+        alt=" "
+        className={styles.bigPlus}
+        onClick={createTaskHandler}
+      />
     </>
   );
 }
